ffbe66d49cd1a5528caa7d9e06366cf6980c1aed
Matt Deng <100417163+mattd233@users.noreply.github.com>
2023-09-19 20:15:52 +1000

    init

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const email = ref();
+const date = ref();
+</script>
+
+<template>
+  <UForm class="container">
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="email" />
+    </UFormGroup>
+    <UFormGroup label="Date" name="password">
+      <VueDatePicker v-model="date"></VueDatePicker>
+    </UFormGroup>
+    <UButton type="submit">
+      Submit
+    </UButton>
+  </UForm>
+</template>
+
+
+<style>
+.container {
+  width: 200px;
+}
+</style>
